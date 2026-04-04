133b536cfd1e0c2b88028de9ddff0d157175c637
Luis Gil <luisfrancisco17msn@gmail.com>
2026-04-03 20:47:51 -0500

    Inicio y estructura de app

---
+++ src/app/pages/basic-page/basic-page.ts
@@ -0,0 +1,23 @@
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-page',
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
+  templateUrl: './basic-page.html',
+})
+export default class BasicPage {
+  nameLower = signal('luis');
+  nameUpper = signal('FRANCISCO');
+  fullName = signal('luIS FrAnCISco');
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanup) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+    }, 1000);
+    onCleanup(() => {
+      clearInterval(interval);
+    });
+  });
+}
